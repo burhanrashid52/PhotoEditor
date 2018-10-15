7dea6bab1e0f53601daf6847a9fa4ff28621e2dc
Anders Lunde <anders@zedge.net>
2018-10-15 13:14:18 +0200

    Add flip colors on double tap

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/PhotoEditor.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/PhotoEditor.java
@@ -63,7 +63,6 @@ public class PhotoEditor implements BrushViewChangeListener, MultiTouchListener.
     private int transparentPixelsClickThroughRadius;
     private Typeface mDefaultTextTypeface;
     private Typeface mDefaultEmojiTypeface;
-    private int hueValue = 0;
     private ColorFilter colorFilter = new ColorMatrixColorFilter(NEGATIVE);
 
 
@@ -120,9 +119,6 @@ public class PhotoEditor implements BrushViewChangeListener, MultiTouchListener.
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onDoubleTap() {
-                int hue = getHueValue();
-                Log.d("PE", "HUE: " + hue);
-//                imageView.setColorFilter(newColorMatrix(hue));
                 if(imageView.getColorFilter() == colorFilter) {
                     imageView.clearColorFilter();
                 } else {
@@ -153,54 +149,6 @@ public class PhotoEditor implements BrushViewChangeListener, MultiTouchListener.
             0,     0, -1.0f,    0, 255, // blue
             0,     0,     0, 1.0f,   0  // alpha
     };
-
-//  drawable.setColorFilter(new ColorMatrixColorFilter(NEGATIVE));
-
-    //Returns hue value between 0 and 360
-    private int getHueValue(){
-        if((hueValue + 60) <= 360){
-            return  hueValue += 60;
-        }
-        return hueValue = 0;
-    }
-    /**
-     * Creates a HUE ajustment ColorFilter
-     * @param value degrees to shift the hue.
-     * @return
-     */
-    public static ColorFilter newColorMatrix(float value )
-    {
-        ColorMatrix cm = new ColorMatrix();
-        adjustHue(cm, value);
-
-        return new ColorMatrixColorFilter(cm);
-    }
-
-    /**
-     * @param cm
-     * @param value
-     */
-    public static void adjustHue(ColorMatrix cm, float value)
-    {
-        value = cleanValue(value, 180f) / 180f * (float) Math.PI;
-        if (value == 0)
-        {
-            return;
-        }
-        float cosVal = (float) Math.cos(value);
-        float sinVal = (float) Math.sin(value);
-        float lumR = 0.213f;
-        float lumG = 0.715f;
-        float lumB = 0.072f;
-        float[] mat = new float[]
-                {
-                        lumR + cosVal * (1 - lumR) + sinVal * (-lumR), lumG + cosVal * (-lumG) + sinVal * (-lumG), lumB + cosVal * (-lumB) + sinVal * (1 - lumB), 0, 0,
-                        lumR + cosVal * (-lumR) + sinVal * (0.143f), lumG + cosVal * (1 - lumG) + sinVal * (0.140f), lumB + cosVal * (-lumB) + sinVal * (-0.283f), 0, 0,
-                        lumR + cosVal * (-lumR) + sinVal * (-(1 - lumR)), lumG + cosVal * (-lumG) + sinVal * (lumG), lumB + cosVal * (1 - lumB) + sinVal * (lumB), 0, 0,
-                        0f, 0f, 0f, 1f, 0f,
-                        0f, 0f, 0f, 0f, 1f };
-        cm.postConcat(new ColorMatrix(mat));
-    }
 
     protected static float cleanValue(float p_val, float p_limit)
     {
