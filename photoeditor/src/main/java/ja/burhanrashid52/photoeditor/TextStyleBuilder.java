159455dee83b1a4638b37aa552b553730933e595
Christian Olcina <christian.olcina@sngular.team>
2019-05-16 09:52:48 +0200

    Add JavaDoc Improve code style

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/TextStyleBuilder.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/TextStyleBuilder.java
@@ -4,44 +4,93 @@ import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * <p>
+ * This class is used to wrap the styles to apply on the TextView on {@link PhotoEditor#addText(String, TextStyleBuilder)} and {@link PhotoEditor#editText(View, String, TextStyleBuilder)}
+ * </p>
+ *
+ * @author <a href="https://github.com/Sulfkain">Christian Caballero</a>
+ * @since 14/05/2019
+ */
 public class TextStyleBuilder {
 
     private Map<TextStyle, Object> values = new HashMap<>();
     protected Map<TextStyle, Object> getValues() { return values; }
 
+    /**
+     * Set this textSize style
+     *
+     * @param size Size to apply on text
+     */
     public void withTextSize(@NonNull float size) {
         values.put(TextStyle.SIZE, size);
     }
 
+    /**
+     * Set this color style
+     *
+     * @param color Color to apply on text
+     */
     public void withTextColor(@NonNull int color) {
         values.put(TextStyle.COLOR, color);
     }
 
+    /**
+     * Set this {@link Typeface} style
+     *
+     * @param textTypeface TypeFace to apply on text
+     */
     public void withTextFont(@NonNull Typeface textTypeface) {
         values.put(TextStyle.FONT_FAMILY, textTypeface);
     }
 
+    /**
+     * Set this gravity style
+     *
+     * @param gravity Gravity style to apply on text
+     */
     public void withGravity(@NonNull int gravity) {
         values.put(TextStyle.GRAVITY, gravity);
     }
 
+    /**
+     * Set this background color
+     *
+     * @param background Background color to apply on text, this method overrides the preview set on {@link TextStyleBuilder#withBackgroundDrawable(Drawable)}
+     */
     public void withBackgroundColor(@NonNull int background) {
         values.put(TextStyle.BACKGROUND, background);
     }
 
+    /**
+     * Set this background {@link Drawable}, this method overrides the preview set on {@link TextStyleBuilder#withBackgroundColor(int)}
+     *
+     * @param bgDrawable Background drawable to apply on text
+     */
     public void withBackgroundDrawable(@NonNull Drawable bgDrawable) {
         values.put(TextStyle.BACKGROUND, bgDrawable);
     }
 
+    /**
+     * Set this textAppearance style
+     *
+     * @param textAppearance Text style to apply on text
+     */
     public void withTextAppearance(@NonNull int textAppearance) {
         values.put(TextStyle.TEXT_APPEARANCE, textAppearance);
     }
 
+    /**
+     * Method to apply all the style setup on this Builder}
+     *
+     * @param textView TextView to apply the style
+     */
     void applyStyle(@NonNull TextView textView) {
         for (Map.Entry<TextStyle, Object> entry : values.entrySet()) {
             switch (entry.getKey()) {
@@ -128,13 +177,16 @@ public class TextStyleBuilder {
         }
     }
 
+    /**
+     * Enum to maintain current supported style properties used on on {@link PhotoEditor#addText(String, TextStyleBuilder)} and {@link PhotoEditor#editText(View, String, TextStyleBuilder)}
+     */
     protected enum TextStyle {
         SIZE("TextSize"),
         COLOR("TextColor"),
-        GRAVITY("GRAVITY"),
-        FONT_FAMILY("FONT_FAMILY"),
+        GRAVITY("Gravity"),
+        FONT_FAMILY("FontFamily"),
         BACKGROUND("Background"),
-        TEXT_APPEARANCE("TEXT_APPEARANCE");
+        TEXT_APPEARANCE("TextAppearance");
 
         TextStyle(String property) {
             this.property = property;
