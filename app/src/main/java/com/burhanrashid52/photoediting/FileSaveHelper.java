34659b8b8c0fa1928aa9df9d777de7b3c79c2320
S Tanveer Hussain <tanveer1807@yahoo.com>
2021-03-23 23:31:15 +0500

    Added Comments
    code explaination

--- app/src/main/java/com/burhanrashid52/photoediting/FileSaveHelper.java
+++ app/src/main/java/com/burhanrashid52/photoediting/FileSaveHelper.java
@@ -19,6 +19,24 @@ import java.io.OutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * General contract of this class is to
+ * create a file on devices running android 10 and above.
+ * As android 10 introduced scope storage
+ * </br>
+ * <p>
+ * How to Use it-
+ * Call {@linkplain FileSaveHelper#createFileForSdk29orHigher(String, OnFileCreateResult)}
+ * if file is created you would receive it's file path and Uri
+ * and after you are done with File call {@linkplain FileSaveHelper#notifyThatFileIsNowPubliclyAvailable()}
+ * <p>
+ * </br>
+ * <p>
+ * It's designed to avoid using libraries ,handler and methods like {@linkplain android.app.Activity#runOnUiThread(Runnable)}.
+ * Remember it observes the lifecycle of {@linkplain AppCompatActivity} in order to shutdown the executor.
+ * </p>
+ *
+ */
 public class FileSaveHelper implements LifecycleObserver {
     private ContentResolver contentResolver;
     private ExecutorService executor;
@@ -55,6 +73,16 @@ public class FileSaveHelper implements LifecycleObserver {
     }
 
 
+    /**
+     * The effect of this method is
+     * 1- insert new Image File data in MediaStore.Images column
+     * 2- create File on Disk.
+     * Remember we had exclusive access to the file and only our app can see the file
+     * unless {@linkplain FileSaveHelper#notifyThatFileIsNowPubliclyAvailable()} is called on.
+     *
+     * @param fileNameToSave fileName
+     * @param listener       result listener
+     */
     @RequiresApi(api = Build.VERSION_CODES.Q)
     public void createFileForSdk29orHigher(String fileNameToSave, OnFileCreateResult listener) {
         this.resultListener = listener;
@@ -62,6 +90,7 @@ public class FileSaveHelper implements LifecycleObserver {
             Cursor cursor = null;
             String filePath = null;
             try {
+                // insert new image file data into media store
                 Uri imageCollection;
                 imageCollection = MediaStore.Images.Media
                         .getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
@@ -70,7 +99,7 @@ public class FileSaveHelper implements LifecycleObserver {
                 newImageDetails.put(MediaStore.Images.Media.IS_PENDING, 1);
                 final Uri editedImageUri = contentResolver
                         .insert(imageCollection, newImageDetails);
-                // create a file . simply File#createNewFile() won't work. simply to meet library needs.
+                // create a file on disk . simply File#createNewFile() won't work. simply to meet library needs.
                 final OutputStream outputStream = contentResolver.openOutputStream(editedImageUri);
                 outputStream.close();
                 String[] proj = {MediaStore.Images.Media.DATA};
@@ -91,6 +120,11 @@ public class FileSaveHelper implements LifecycleObserver {
 
     }
 
+    /**
+     * The general contract of this method is to notify that
+     * file is visible now and can be accessed by all other entities that has access to File System.
+     * call it when you are done processing the file.
+     */
     @RequiresApi(api = Build.VERSION_CODES.Q)
     public void notifyThatFileIsNowPubliclyAvailable() {
         executor.submit(() -> {
@@ -110,7 +144,9 @@ public class FileSaveHelper implements LifecycleObserver {
         public Uri uri;
         public String error;
 
-        public FileCreateData(boolean isCreated, String filePath, Uri uri, String error, ContentValues newImageDetails) {
+        public FileCreateData(boolean isCreated, String filePath,
+                              Uri uri, String error,
+                              ContentValues newImageDetails) {
             this.isCreated = isCreated;
             this.filePath = filePath;
             this.uri = uri;
@@ -120,11 +156,17 @@ public class FileSaveHelper implements LifecycleObserver {
     }
 
     public interface OnFileCreateResult {
+        /**
+         * @param created  whether file creation is success or failure
+         * @param filePath filepath on disk. null in case of failure
+         * @param error    in case file creation is failed . it would represent the cause
+         * @param Uri      Uri to the newly created file. null in case of failure
+         */
         void onFileCreateResult(boolean created, String filePath, String error, Uri Uri);
     }
 
     private void updateResult(boolean result, String filePath, String error, Uri uri, ContentValues newImageDetails) {
-        fileCreatedResult.postValue(new FileCreateData(result, filePath, uri, error,newImageDetails));
+        fileCreatedResult.postValue(new FileCreateData(result, filePath, uri, error, newImageDetails));
     }
 
 }
