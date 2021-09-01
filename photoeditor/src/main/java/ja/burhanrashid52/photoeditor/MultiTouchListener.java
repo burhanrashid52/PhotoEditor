e6b14c061162150281bba8423aa83a9edd77f554
Luciano Cheng <luciano.cheng@gmail.com>
2021-09-01 14:42:49 -0700

    Remove automated bring to front

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/MultiTouchListener.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/MultiTouchListener.java
@@ -268,10 +268,8 @@ public class MultiTouchListener implements OnTouchListener {
                         deleteView.setVisibility(View.VISIBLE);
                     }
 
-                    // NOTE(cheng): Re-enabling since there was a uninstall spike / ranking dropoff
-                    //              after disabling this.
-                    // TODO(cheng): Turn this off once we have the "bringToFront()" button existing.
-                    view.bringToFront();
+                    // NOTE(cheng): Disabling this since this is a button now
+                    // view.bringToFront();
                     firePhotoEditorSDKListener(
                             view,
                             PhotoEditorSDKListenerMode.START_VIEW_CHANGE
