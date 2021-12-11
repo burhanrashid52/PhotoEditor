c27af2e0b3632687c5e9512eb50b28f73554ec4c
Luciano Cheng <luciano.cheng@gmail.com>
2021-12-10 19:12:14 -0800

    Convert PhotoApp to kotlin

---
+++ app/src/main/java/com/burhanrashid52/photoediting/base/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.burhanrashid52.photoediting.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * @author [Burhanuddin Rashid](https://github.com/burhanrashid52)
+ * @version 0.1.2
+ * @since 5/25/2018
+ */
+abstract class BaseFragment : Fragment() {
+    protected abstract val layoutId: Int
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        require(layoutId != 0) { "Invalid layout id" }
+        return inflater.inflate(layoutId, container, false)
+    }
+}
---
+++ app/src/main/java/com/burhanrashid52/photoediting/tools/EditingToolsAdapter.kt
@@ -0,0 +1,68 @@
+package com.burhanrashid52.photoediting.tools
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.burhanrashid52.photoediting.R
+import java.util.ArrayList
+
+/**
+ * @author [Burhanuddin Rashid](https://github.com/burhanrashid52)
+ * @version 0.1.2
+ * @since 5/23/2018
+ */
+class EditingToolsAdapter(private val mOnItemSelected: OnItemSelected) :
+    RecyclerView.Adapter<EditingToolsAdapter.ViewHolder>() {
+    private val mToolList: MutableList<ToolModel> = ArrayList()
+
+    interface OnItemSelected {
+        fun onToolSelected(toolType: ToolType?)
+    }
+
+    internal inner class ToolModel(
+        val mToolName: String,
+        val mToolIcon: Int,
+        val mToolType: ToolType
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_editing_tools, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mToolList[position]
+        holder.txtTool.text = item.mToolName
+        holder.imgToolIcon.setImageResource(item.mToolIcon)
+    }
+
+    override fun getItemCount(): Int {
+        return mToolList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgToolIcon: ImageView = itemView.findViewById(R.id.imgToolIcon)
+        val txtTool: TextView = itemView.findViewById(R.id.txtTool)
+
+        init {
+            itemView.setOnClickListener { _: View? ->
+                mOnItemSelected.onToolSelected(
+                    mToolList[layoutPosition].mToolType
+                )
+            }
+        }
+    }
+
+    init {
+        mToolList.add(ToolModel("Shape", R.drawable.ic_oval, ToolType.SHAPE))
+        mToolList.add(ToolModel("Text", R.drawable.ic_text, ToolType.TEXT))
+        mToolList.add(ToolModel("Eraser", R.drawable.ic_eraser, ToolType.ERASER))
+        mToolList.add(ToolModel("Filter", R.drawable.ic_photo_filter, ToolType.FILTER))
+        mToolList.add(ToolModel("Emoji", R.drawable.ic_insert_emoticon, ToolType.EMOJI))
+        mToolList.add(ToolModel("Sticker", R.drawable.ic_sticker, ToolType.STICKER))
+    }
+}
