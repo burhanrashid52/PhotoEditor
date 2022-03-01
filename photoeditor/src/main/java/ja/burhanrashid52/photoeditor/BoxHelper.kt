7697faccfa65e886a9672f25e32493765e09864a
Luciano Cheng <luciano.cheng@gmail.com>
2022-02-28 22:41:52 -0800

    Finish off graphic logic conversion to kotlin

---
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/BoxHelper.kt
@@ -0,0 +1,42 @@
+package ja.burhanrashid52.photoeditor
+
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.FrameLayout
+import android.widget.ImageView
+
+/**
+ * Created by Burhanuddin Rashid on 18/05/21.
+ *
+ * @author <https:></https:>//github.com/burhanrashid52>
+ */
+internal class BoxHelper(
+    private val mCanvasView: RelativeLayout,
+    private val mViewState: PhotoEditorViewState
+) {
+    fun clearHelperBox() {
+        for (i in 0 until mCanvasView.childCount) {
+            val childAt = mCanvasView.getChildAt(i)
+            val frmBorder = childAt.findViewById<FrameLayout>(R.id.frmBorder)
+            frmBorder?.setBackgroundResource(0)
+            val imgClose: ImageView? = null
+            imgClose?.visibility = View.GONE
+        }
+        mViewState.clearCurrentSelectedView()
+    }
+
+    fun clearAllViews(drawingView: DrawingView?) {
+        for (i in 0 until mViewState.addedViewsCount) {
+            mCanvasView.removeView(mViewState.getAddedView(i))
+        }
+        drawingView?.let {
+            if (mViewState.containsAddedView(it)) {
+                mCanvasView.addView(it)
+            }
+        }
+
+        mViewState.clearAddedViews()
+        mViewState.clearRedoViews()
+        drawingView?.clearAll()
+    }
+}
