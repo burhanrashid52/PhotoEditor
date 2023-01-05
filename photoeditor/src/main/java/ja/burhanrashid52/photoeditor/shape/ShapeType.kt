7748ee8caaf4f7207ed5d5f2c2fa8d9d3672a300
Michal Wolski <wasky@users.noreply.github.com>
2023-01-05 21:33:19 +0100

    Change ShapeType class to interface

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/shape/ShapeType.kt
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/shape/ShapeType.kt
@@ -3,15 +3,12 @@ package ja.burhanrashid52.photoeditor.shape
 /**
  * The different kind of known Shapes.
  */
-sealed class ShapeType {
+sealed interface ShapeType {
 
-    object Brush : ShapeType()
-    object Oval : ShapeType()
-    object Rectangle : ShapeType()
-    object Line : ShapeType()
-
-    class Arrow(
-        val pointerPosition: ArrowPointerPosition = ArrowPointerPosition.START
-    ) : ShapeType()
+    object Brush : ShapeType
+    object Oval : ShapeType
+    object Rectangle : ShapeType
+    object Line : ShapeType
+    class Arrow(val pointerPosition: ArrowPointerPosition = ArrowPointerPosition.START) : ShapeType
 
 }
